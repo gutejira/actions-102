61a990f41a8c05e83b8a360cfd7bf998eac0be19
gutejira <gutejirajara@gmail.com>
2025-12-26 12:23:17 -0800

    fixed shell script

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,7 +1,14 @@
-	sudo apt-get install -y cowsay
-	cowsay -f elephant "Run for the cover, I am a elephant!" >> elephant.txt
-	run: grep -i "elephant" elephant.txt
-	run: cat elephant.txt
-	echo "My first GitHub Actions Workflow!"
-	ls-ltra
-	cat README.md
+#!/bin/bash
+set -e
+
+sudo apt-get update
+sudo apt-get install -y cowsay
+
+cowsay -f elephant "Run for the cover, I am an elephant!" >> elephant.txt
+
+grep -i "elephant" elephant.txt
+cat elephant.txt
+
+echo "My first GitHub Actions Workflow!"
+ls -ltra
+cat README.md
